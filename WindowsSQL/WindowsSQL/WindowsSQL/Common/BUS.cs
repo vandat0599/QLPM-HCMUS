8043ffffa687a297563fba7314ffa75ca62cd6dc
Dat Nguyen <tiendat3550@gmail.com>
2020-08-23 00:39:42 +0700

    create DAO, BUS

---
+++ WindowsSQL/WindowsSQL/WindowsSQL/Common/BUS.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsSQL.Common
+{
+    interface BUS<T>
+    {
+        void create(T t);
+        void update(T t);
+        List<T> getAll();
+        void delete(T t);
+    }
+}
